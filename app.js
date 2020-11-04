1ccd421c9130fb9aa2525896dccad19a84eb7f3a
Emily Scott <62396942+escott2@users.noreply.github.com>
2020-11-04 12:34:59 -0600

    Add user input and buttons

---
+++ app.js
@@ -0,0 +1,24 @@
+const gameWordArray = ["environment", "watermelon", "maple","meadow", "autumn", "grass", "season", "sunshine", "temperature"];
+
+const playBtn = document.querySelector(".js-play-btn");
+const submitBtn = document.querySelector(".js-submit-btn");
+
+//Event Listeners**************************************************
+
+playBtn.addEventListener("click", () => {
+    //1. generate random number, to the length of the gameWordArray
+
+    //2. use index to choose word from gameWordArray, save in variable
+
+    //3. generate dashes in word-container, same length as chosen word, in variable.
+    
+});
+
+submitBtn.addEventListener("click", () => {
+    /*1. Letter submitted checked for in the array.
+    String methods and array methods to do this,
+    is it better to work with a string or array?*/
+
+    //2. Logic required, two paths  
+
+});
