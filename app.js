88c686739b81b2a157f8c027b46c1ee5b7c9d854
Emily Scott <62396942+escott2@users.noreply.github.com>
2020-11-04 21:18:03 -0600

    Add game logic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const gameWordArray = ["environment", "watermelon", "maple","meadow", "autumn", "grass", "season", "sunshine", "temperature"];
+const gameWordArray = ["environment", "watermelon", "maple", "meadow", "autumn", "grass", "season", "sunshine", "temperature"];
 
 const playBtn = document.querySelector(".js-play-btn");
 const submitBtn = document.querySelector(".js-submit-btn");
+let gameWord = "";
+
 
 //Functions *******************************************************
 function countArrayItems(array) {
@@ -20,23 +22,30 @@ playBtn.addEventListener("click", () => {
     const getRandomIndex = getRandomInt(countWords);
 
     //2. use index to choose word from gameWordArray, save in variable
-    const gameWord = gameWordArray[getRandomIndex];
-    
+    gameWord = gameWordArray[getRandomIndex].toUpperCase();
+
     //3. generate dashes in word-container, same length as chosen word, in variable.
     const wordLength = gameWord.length;
-    let wordPlaceholder = "";
-    for (let i = 0; i < wordLength; i++) {
-        wordPlaceholder += "_ ";
+    // let wordPlaceholder = "";
+    // for (let i = 0; i < wordLength; i++) {
+    //     wordPlaceholder += "_ ";
+    // }
+
+    const wordPlaceholderArray = [];
+    for (let i = 0; i < wordLength; i++)  {
+        wordPlaceholderArray.push("_");
     }
+
     console.log(gameWord);
 
-    document.querySelector(".js-word-container").textContent = wordPlaceholder;
+    // document.querySelector(".js-word-container").textContent = wordPlaceholder;
+    document.querySelector(".js-word-container").textContent = wordPlaceholderArray.join(" ");
+
 
     //4. display content
-        document.querySelector(".js-display-game").classList.remove("d-none");
-        document.querySelector(".js-display-game").classList.add("d-block");
+    document.querySelector(".js-display-game").classList.remove("d-none");
+    document.querySelector(".js-display-game").classList.add("d-block");
 
-    
 });
 
 submitBtn.addEventListener("click", () => {
@@ -44,6 +53,30 @@ submitBtn.addEventListener("click", () => {
     String methods and array methods to do this,
     is it better to work with a string or array?*/
 
+    let guessLetter = document.getElementById("guess-input").value.toUpperCase();
+    console.log(guessLetter);
+
     //2. Logic required, two paths  
+
+    //if letter is in word
+    const letterPositionIndex = [];
+    
+
+    if (gameWord.split("").includes(guessLetter)) {
+        for (let i = 0; i < gameWord.length; i++) {
+            if (guessLetter === gameWord[i].toUpperCase()) {
+                letterPositionIndex.push(i);
+            }
+    }
+
+    console.log(letterPositionIndex);
+
+    //guess letter(s) placed in placeholder array.
+
+    //if letter is not in word
+        //change UI & decrement score...
+
+
+    }
 
 });
